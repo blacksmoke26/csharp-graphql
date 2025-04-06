9987830d7eba48333711f9e3b407b1d3a75bf09f
Junaid Atari <mj.atari@gmail.com>
2025-04-06 18:57:01 +0500

    feat: create abstraction class

---
+++ src/Abstraction/Inputs/Auth/LoginCredentialInput.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/csharp-graphql
+
+namespace Abstraction.Inputs.Auth;
+
+[GraphQLDescription("Use to login the existing user with credentials")]
+[InputObjectType]
+public struct LoginCredentialInput {
+  [GraphQLDescription("The email address")]
+  public string Email { get; init; }
+
+  [GraphQLDescription("The password")]
+  public string Password { get; init; }
+}
---
+++ src/Abstraction/RequestExecutorBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+using HotChocolate.Execution.Configuration;
+
+namespace Abstraction;
+
+public static class RequestExecutorBuilderExtensions {
+  /// <summary>
+  /// Register GraphQL specific abstractions
+  /// </summary>
+  /// <param name="builder">The <see cref="HotChocolate.Execution.Configuration.IRequestExecutorBuilder"/> instance</param>
+  /// <returns>The <see cref="HotChocolate.Execution.Configuration.IRequestExecutorBuilder"/></returns>
+  public static IRequestExecutorBuilder AddAbstractions(this IRequestExecutorBuilder builder) {
+    return builder;
+  }
+}
