bc9bbbc6a247f402f0105cdf94a281cf4eeae812
Junaid Atari <mj.atari@gmail.com>
2025-04-08 20:48:32 +0500

    refactor: code cleanup

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -2,11 +2,9 @@
 // Copyright (c) 2025 Junaid Atari, and contributors
 // Repository:https://github.com/blacksmoke26/csharp-graphql
 
-using Server.Core.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.InitBootstrapper(builder.Configuration);
 
+builder.Services.InitBootstrapper(builder.Configuration);
 var app = builder.Build();
 
 await app.InitializeDbAsync();
@@ -14,30 +12,3 @@ await app.InitializeDbAsync();
 app.UseBootstrapper();
 
 app.Run();
-
-/*[GraphQLDescription("The current user information")]
-public record Me {
-  [GraphQLDescription("The first name of the user")]
-  public string FirstName { get; set; }
-}*/
-
-/*[QueryType]
-public class Query {
-  public async Task<Me> Me(UserRepository userRepo) {
-    var user = await userRepo.GetAsync(new FindOptions<User, Me> {
-      /*Project = x => new Me {
-        FirstName = x.FirstName
-      },#1#
-      Where = [u => u.Id == 1]
-    });
-
-    return user;
-  }
-}
-
-[QueryType]
-public class Users {
-  [UsePaging]
-  [UseProjection]
-  public IQueryable<User> GetAllUsers(ApplicationDbContext context) => context.Users;
-}*/
