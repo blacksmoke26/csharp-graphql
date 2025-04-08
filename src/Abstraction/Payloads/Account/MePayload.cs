1920f8528f2f46b017d612eba64b970d55113c73
Junaid Atari <mj.atari@gmail.com>
2025-04-08 22:06:37 +0500

    feat(dto): create payload struct

---
+++ src/Abstraction/Payloads/Account/MePayload.cs
@@ -0,0 +1,28 @@
+﻿namespace Abstraction.Payloads.Account;
+
+[GraphQLDescription("This class formats the successful user details response")]
+public struct MePayload {
+  [GraphQLDescription("The unique identifier")]
+  public required long Id { get; set; }
+
+  [GraphQLDescription("The user's first and last name")]
+  public required string Fullname { get; init; }
+
+  [GraphQLDescription("The user's first name")]
+  public required string FirstName { get; init; }
+
+  [GraphQLDescription("The user's last name")]
+  public required string LastName { get; init; }
+
+  [GraphQLDescription("The email address")]
+  public required string Email { get; init; }
+
+  [GraphQLDescription("The role name")]
+  public required string Role { get; init; }
+
+  [GraphQLDescription("Status")]
+  public required string Status { get; set; }
+
+  [GraphQLDescription("Creation date")]
+  public required DateTime? CreatedAt { get; set; }
+}
