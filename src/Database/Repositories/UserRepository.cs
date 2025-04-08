800507ce7c26dbe5f615ef94cdd82a54bfb52c4d
Junaid Atari <mj.atari@gmail.com>
2025-04-08 20:38:52 +0500

    feat(repo): create utility methods to retrieve entity

--- src/Database/Repositories/UserRepository.cs
+++ src/Database/Repositories/UserRepository.cs
@@ -7,4 +7,23 @@ using Database.Core.Base;
 namespace Database.Repositories;
 
 public sealed class UserRepository(ApplicationDbContext context) : RepositoryBase<User>(context) {
+  /// <summary>
+  /// Fetch a single entity against authorization key
+  /// </summary>
+  /// <param name="authKey">The authorization key</param>
+  /// <param name="token">Cancellation token</param>
+  /// <returns>A task that represents the asynchronous operation. Either an entity or null upon none</returns>
+  public Task<User?> GetByAuthKeyAsync(string authKey, CancellationToken token = default) {
+    return DbSet.AsNoTracking().FirstOrDefaultAsync(x => x.AuthKey == authKey, token);
+  }
+
+  /// <summary>
+  /// Fetch a single entity against email address
+  /// </summary>
+  /// <param name="email">The email address</param>
+  /// <param name="token">Cancellation token</param>
+  /// <returns>A task that represents the asynchronous operation. Either an entity or null upon none</returns>
+  public Task<User?> GetByEmailAsync(string email, CancellationToken token = default) {
+    return DbSet.AsNoTracking().FirstOrDefaultAsync(x => x.Email == email, token);
+  }
 }
