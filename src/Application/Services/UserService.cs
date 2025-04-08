fa94f755e207205362521a96e1d3d2d852809aae
Junaid Atari <mj.atari@gmail.com>
2025-04-08 22:07:53 +0500

    feat(service): create method to change account password

--- src/Application/Services/UserService.cs
+++ src/Application/Services/UserService.cs
@@ -9,5 +9,19 @@ public struct LoginOptions {
   public string? IpAddress { get; init; }
 }
 
-public class UserService {
+public class UserService(
+  UserRepository userRepository) : ServiceBase {
+  /// <summary>
+  /// Change the account password
+  /// </summary>
+  /// <param name="user">The user object</param>
+  /// <param name="newPassword">The password to change</param>
+  /// <param name="token">The cancellation token</param>
+  /// <returns>Whatever the password has changed or not</returns>
+  public async Task<bool> ChangePasswordAsync(User user, string newPassword, CancellationToken token = default) {
+    user.SetPassword(newPassword);
+    user.OnPasswordUpdate();
+    user.GenerateAuthKey();
+    return await userRepository.UpdateAsync(user, token) > 0;
+  }
 }
