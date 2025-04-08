3e82a2eea7ac3a4ef213a5254ac3784c49b36cd0
Junaid Atari <mj.atari@gmail.com>
2025-04-08 22:04:01 +0500

    refactor: move file, update references

--- src/Server/Resolvers/Auth/Mutations/LogoutMutation.cs
+++ src/Server/Resolvers/Auth/Mutations/LogoutMutation.cs
@@ -2,16 +2,13 @@
 // Copyright (c) 2025 Junaid Atari, and contributors
 // Repository: https://github.com/blacksmoke26/csharp-graphql
 
-using Application.Constants;
-using Application.Services;
-
-namespace Server.Resolvers.Auth;
+namespace Server.Resolvers.Auth.Mutations;
 
 [MutationType]
 public static class LogoutMutation {
   [GraphQLDescription("Logouts the user and invalidate the token")]
   [Authorize(Policy = AuthPolicies.AuthPolicy)]
-  public static async Task<bool> Logout(
+  public static async Task<bool> AuthLogout(
     IdentityService idService,
     IHttpContextAccessor httpContext, CancellationToken token
   ) {
