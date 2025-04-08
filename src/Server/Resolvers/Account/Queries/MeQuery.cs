769f6f1f8f37a2a6600d3248e27a60a2cc8140fc
Junaid Atari <mj.atari@gmail.com>
2025-04-08 22:04:45 +0500

    feat(schema): create account specific query and mutation

---
+++ src/Server/Resolvers/Account/Queries/MeQuery.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/csharp-graphql
+
+using Abstraction.Payloads.Account;
+
+namespace Server.Resolvers.Account.Queries;
+
+[QueryType]
+[Authorize(Policy = AuthPolicies.AuthPolicy)]
+public static class MeQuery {
+  public static Task<MePayload> Me(IHttpContextAccessor httpContext) {
+    var user = httpContext.HttpContext!.GetUser();
+    
+    return Task.FromResult(new MePayload {
+      Id = user.Id,
+      Fullname = user.FullName,
+      FirstName = user.FirstName,
+      LastName = user.LastName,
+      Email = user.Email,
+      Role = user.Role,
+      Status = user.Status.ToString(),
+      CreatedAt = user.CreatedAt
+    });
+  }
+}
