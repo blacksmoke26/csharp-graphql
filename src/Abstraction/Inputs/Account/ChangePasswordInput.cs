46a09a4bb0d4282cfdf837726043cd711509a89b
Junaid Atari <mj.atari@gmail.com>
2025-04-08 22:05:47 +0500

    feat(validator): create input DTO and validator

---
+++ src/Abstraction/Inputs/Account/ChangePasswordInput.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/csharp-graphql
+
+namespace Abstraction.Inputs.Account;
+
+[GraphQLDescription("Use to change the account password")]
+[InputObjectType]
+public struct ChangePasswordInput {
+  [GraphQLDescription("The current password")] [property: Range(8, 20)]
+  public string CurrentPassword { get; init; }
+
+  [GraphQLDescription("The new password to update")] [property: Range(8, 20)]
+  public string NewPassword { get; init; }
+}
---
+++ src/Application/Domain/Validators/Account/ChangePasswordInputValidator.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+using Abstraction.Inputs.Account;
+using Application.Extensions;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Domain.Validators.Account;
+
+/// <summary>
+/// Validator for change password request.
+/// </summary>
+/// <remarks>
+/// This validator ensures that the password reset payload meets the required criteria:
+/// - Current password is valid and not empty
+/// - New password is valid and not empty
+/// </remarks>
+public class ChangePasswordInputValidator : AbstractValidator<ChangePasswordInput> {
+  public ChangePasswordInputValidator(IHttpContextAccessor context) {
+    RuleFor(x => x.CurrentPassword)
+      .MinimumLength(6)
+      .MaximumLength(20)
+      .Must(value => context.HttpContext!.GetIdentity().User.ValidatePassword(value ?? string.Empty))
+      .WithMessage("The current password is not valid")
+      .NotEmpty();
+
+    RuleFor(x => x.NewPassword)
+      .MinimumLength(8)
+      .MaximumLength(20)
+      .Must((payload, value) => payload.CurrentPassword != (value ?? string.Empty))
+      .WithMessage("The new password should be different")
+      .NotEmpty();
+  }
+}
