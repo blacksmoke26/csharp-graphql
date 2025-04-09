ce085d64a724a32666079c19177ba8c7df1d6c4a
Junaid Atari <mj.atari@gmail.com>
2025-04-09 21:23:30 +0500

    feat(ext): create Movie entity extension methods

--- src/Database/Core/Extensions/MovieEntityExtensions.cs
+++ src/Database/Core/Extensions/MovieEntityExtensions.cs
@@ -6,4 +6,17 @@ namespace Database.Core.Extensions;
 
 /// The class represents the extensions methods for 'Movie' entity
 public static class MovieEntityExtensions {
+  /// <summary>
+  /// Generates a slug against current values
+  /// </summary>
+  public static void GenerateSlug(this Movie movie) {
+    movie.Slug = GenerateSlug(movie, movie.Title, movie.YearOfRelease);
+  }
+
+  /// <summary>
+  /// Generates a slug based on a movie name and the year of release
+  /// </summary>
+  public static string GenerateSlug(this Movie movie, string title, short yearOfRelease) {
+    return movie.Slug = StringHelper.Slugify(title, " ", yearOfRelease);
+  }
 }
