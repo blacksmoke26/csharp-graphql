5fb9e57cd730dc124232d25b95d37693271efeda
Junaid Atari <mj.atari@gmail.com>
2025-04-10 20:33:19 +0500

    feat(config): create configuration class

---
+++ src/Application/Config/AuthenticationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/csharp-graphql
+
+namespace Application.Config;
+
+public struct AuthenticationConfiguration {
+  /// <summary>Expires token and regenerates the authentication key after logout</summary>
+  public bool RefreshAuthKeyAfterLogout { get; init; }
+}
