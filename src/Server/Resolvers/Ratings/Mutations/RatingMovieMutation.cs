e40fa6dc36603574f2e0734bbebc4bb3c06d3dc9
Junaid Atari <mj.atari@gmail.com>
2025-04-13 22:56:18 +0500

    feat: implement movie ratings / query, mutations and sorting

---
+++ src/Server/Resolvers/Ratings/Mutations/RatingMovieMutation.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/csharp-graphql
+
+using Abstraction.Inputs.Rating;
+using Application.Domain.Validators.Rating;
+
+namespace Server.Resolvers.Ratings.Mutations;
+
+[MutationType]
+public class RatingMovieMutation {
+  [Authorize(Policy = AuthPolicy.Trusted)]
+  [GraphQLDescription("Rates a single movie")]
+  public Task<bool> RatingMovieAsync(
+    [UseFluentValidation, UseValidator<MovieRatingInputValidator>]
+    MovieRatingInput input,
+    RatingService ratingService,
+    IHttpContextAccessor contextAccessor, CancellationToken token
+  ) {
+    var context = contextAccessor.HttpContext!;
+    return ratingService.RateMovieAsync(context.GetId(), input, token);
+  }
+}
