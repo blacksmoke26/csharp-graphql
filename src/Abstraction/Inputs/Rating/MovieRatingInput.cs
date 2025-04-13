54b8d1a5b2de1fdba874bdaa543d6438c8a7c25a
Junaid Atari <mj.atari@gmail.com>
2025-04-13 22:57:45 +0500

    feat(validator): create DTO and validator

---
+++ src/Abstraction/Inputs/Rating/MovieRatingInput.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstraction.Inputs.Rating;
+
+[GraphQLDescription("Rate a movie")]
+public struct MovieRatingInput {
+  [GraphQLDescription("Movie ID")] [ID]
+  public long MovieId { get; init; }
+
+  [GraphQLDescription("Overall rating")]
+  public short Score { get; init; }
+
+  [GraphQLDescription("Additional feedback")]
+  public string? Feedback { get; init; }
+}
---
+++ src/Application/Domain/Validators/Rating/MovieRatingInputValidator.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
+using Abstraction.Inputs.Rating;
+using FluentValidation;
+
+namespace Application.Domain.Validators.Rating;
+
+public class MovieRatingInputValidator: AbstractValidator<MovieRatingInput> {
+  public MovieRatingInputValidator() {
+    RuleFor(x => x.MovieId)
+      .GreaterThan(0)
+      .NotEmpty();
+
+    RuleFor(x => x.Score)
+      .InclusiveBetween((short)0, (short)5)
+      .NotNull();
+
+    RuleFor(x => x.Feedback)
+      .MaximumLength(500);
+  }
+}
